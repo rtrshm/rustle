bf9dfe110a8788b4357750df4fa6713fd1035211
artur <arturshum@proton.me>
2024-03-13 15:14:41 -0400

    list entries, select listings

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 use crossterm::{
-    terminal::{
-        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
-        LeaveAlternateScreen
-    },
-    ExecutableCommand
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
 };
 use ratatui::prelude::*;
 use std::io::stdout;
 
 pub mod model;
 
-use crate::model::{
-    Model, RunningState, view, handle_event, update
-};
+use crate::model::{handle_event, update, view, Model, RunningState};
 
 fn main() -> color_eyre::Result<()> {
-
     stdout().execute(EnterAlternateScreen)?;
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
@@ -23,9 +17,7 @@ fn main() -> color_eyre::Result<()> {
 
     let mut model = Model::default();
 
-
     while model.running_state != RunningState::Done {
-
         terminal.draw(|frame| view(&mut model, frame))?;
 
         let mut current_message = handle_event(&mut model)?;
@@ -39,4 +31,3 @@ fn main() -> color_eyre::Result<()> {
     disable_raw_mode()?;
     Ok(())
 }
-
--- src/model.rs
+++ src/model.rs
@@ -1,59 +1,55 @@
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::{
-    style::{
-        Style, Color
-    },
     layout::*,
     prelude::*,
+    style::{Color, Style},
+    widgets::calendar::{CalendarEventStore, Monthly},
     widgets::*,
-    widgets::calendar::{
-        CalendarEventStore,
-        Monthly
-    }
 };
-use crossterm::event::{self,
-        Event, 
-        KeyCode, 
-        KeyEventKind};
 
+use std::{
+    fs::{read_dir, ReadDir},
+    path::PathBuf,
+    time::Duration,
+};
+use time::{format_description, Date, OffsetDateTime};
 use tui_textarea::TextArea;
-use std::time::Duration;
-use time::{
-    Date,
-    OffsetDateTime};
 
 #[derive(Debug, Default)]
 pub struct Model<'a> {
     pub running_state: RunningState,
     textarea: TextArea<'a>,
     active_window: ActiveWindow,
-    calendar_state: CalendarState
+    calendar_state: CalendarState,
+    selected_listing: usize,
+    num_listings: usize,
 }
 
 #[derive(Debug, Default, PartialEq, Eq)]
 pub enum RunningState {
     #[default]
     Running,
-    Done
+    Done,
 }
 
 #[derive(Debug, Default, PartialEq)]
-pub enum ActiveWindow { 
+pub enum ActiveWindow {
     #[default]
     Menu,
-    EditBox
+    EditBox,
 }
 
 #[derive(Debug)]
 struct CalendarState {
     enabled: bool,
-    selected_date: Date
+    selected_date: Date,
 }
 
 impl Default for CalendarState {
     fn default() -> CalendarState {
         CalendarState {
             enabled: false,
-            selected_date: OffsetDateTime::now_utc().date()
+            selected_date: OffsetDateTime::now_utc().date(),
         }
     }
 }
@@ -69,30 +65,27 @@ pub enum Message {
     SwitchWindows,
     ToggleCalendar,
     Enter,
-    Other
+    Other,
 }
 
-
 pub fn handle_event(model: &mut Model) -> color_eyre::Result<Option<Message>> {
-    if event::poll(Duration::from_millis(250)) ? {
+    if event::poll(Duration::from_millis(250))? {
         if let Event::Key(key) = event::read()? {
             if key.kind == KeyEventKind::Press {
                 match model.active_window {
                     ActiveWindow::Menu => return Ok(handle_key(key)),
-                    ActiveWindow::EditBox => {
-                        match key.code {
-                            KeyCode::Tab => return Ok(handle_key(key)),
-                            _ => {
-                                model.textarea.input(key);
-                                return Ok(None)
-                            }
-                        }   
-                    }
+                    ActiveWindow::EditBox => match key.code {
+                        KeyCode::Tab => return Ok(handle_key(key)),
+                        _ => {
+                            model.textarea.input(key);
+                            return Ok(None);
+                        }
+                    },
                 }
             }
         }
-    }    
-    Ok(None)    
+    }
+    Ok(None)
 }
 
 fn handle_key(key: event::KeyEvent) -> Option<Message> {
@@ -105,140 +98,178 @@ fn handle_key(key: event::KeyEvent) -> Option<Message> {
         KeyCode::Left => Some(Message::Left),
         KeyCode::Right => Some(Message::Right),
         KeyCode::Enter => Some(Message::Enter),
-        _ => None
+        _ => None,
     }
 }
 
 fn update_date(model: &mut Model, offset_in_days: i64) {
-    let result = model.calendar_state.selected_date.checked_add(time::Duration::days(offset_in_days));
-    match result {
-        Some(result_date) =>
-            if result_date.month() == model.calendar_state.selected_date.month() {
-                model.calendar_state.selected_date = result_date
-            } 
-        _ => ()
-    };
+    let result = model
+        .calendar_state
+        .selected_date
+        .checked_add(time::Duration::days(offset_in_days));
+    if let Some(result_date) = result {
+        if result_date.month() == model.calendar_state.selected_date.month() {
+            model.calendar_state.selected_date = result_date;
+            model.selected_listing = 0;
+            model.num_listings = 0;
+        }
+    }
 }
 
 pub fn update(model: &mut Model, msg: Message) -> Option<Message> {
     match msg {
         Message::Quit => {
             model.running_state = RunningState::Done;
-        },
-        Message::SwitchWindows => {
-            match model.active_window {
-                ActiveWindow::EditBox => {
-                    model.active_window = ActiveWindow::Menu
-                },
-                ActiveWindow::Menu => {
-                    model.active_window = ActiveWindow::EditBox
-                }
-            }
+        }
+        Message::SwitchWindows => match model.active_window {
+            ActiveWindow::EditBox => model.active_window = ActiveWindow::Menu,
+            ActiveWindow::Menu => model.active_window = ActiveWindow::EditBox,
         },
         Message::ToggleCalendar => {
             model.calendar_state.enabled = !model.calendar_state.enabled;
-        },
+        }
 
         Message::Up => {
             if model.calendar_state.enabled {
                 update_date(model, -7);
             }
-        },
+
+            if model.active_window == ActiveWindow::Menu && model.selected_listing > 0 {
+                model.selected_listing -= 1;
+            }
+        }
 
         Message::Down => {
             if model.calendar_state.enabled {
                 update_date(model, 7);
             }
-        },
+
+            if model.active_window == ActiveWindow::Menu
+                && model.num_listings > 0
+                && model.selected_listing < model.num_listings - 1
+            {
+                model.selected_listing += 1
+            }
+        }
 
         Message::Left => {
-            if model.calendar_state.enabled{
+            if model.calendar_state.enabled {
                 update_date(model, -1);
             }
-        },
+        }
 
         Message::Right => {
             if model.calendar_state.enabled {
                 update_date(model, 1)
             }
-        },
+        }
 
         Message::Enter => {
             if model.calendar_state.enabled {
                 model.calendar_state.enabled = !model.calendar_state.enabled;
             }
         }
-        _ => return None
+        _ => return None,
     }
     None
 }
 
-pub fn view(model: &mut Model, f: &mut Frame) {
+fn format_date(date: Date) -> String {
+    let format = format_description::parse("[year]-[month]-[day]").unwrap();
+    date.format(&format).unwrap()
+}
 
+fn list_entries(selected_date: Date) -> Option<ReadDir> {
+    let dirname = format_date(selected_date);
+    let mut path = PathBuf::new();
+    path.push("entries");
+    path.push(dirname);
+
+    match read_dir(path) {
+        Ok(dir_entries) => Some(dir_entries),
+        _ => None,
+    }
+}
+
+pub fn view(model: &mut Model, f: &mut Frame) {
     let layout = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints(vec![
-            Constraint::Ratio(1, 3),
-            Constraint::Ratio(2, 3)
-        ])
+        .constraints(vec![Constraint::Ratio(1, 3), Constraint::Ratio(2, 3)])
         .split(f.size());
 
-    let mut textarea_block = Block::default()
-            .borders(Borders::ALL)
-            .title("textArea");
+    let mut textarea_block = Block::default().borders(Borders::ALL).title("textArea");
 
-    let mut menu_block = Block::default()
-            .borders(Borders::ALL)
-            .title("menu");
+    let mut menu_block = Block::default().borders(Borders::ALL);
 
     match model.active_window {
         ActiveWindow::Menu => {
             menu_block = menu_block.border_style(Style::default().fg(Color::LightBlue));
-        },
+        }
         ActiveWindow::EditBox => {
             textarea_block = textarea_block.border_style(Style::default().fg(Color::LightBlue));
         }
     };
 
-    model.textarea.set_block(
-        textarea_block
-    );
+    model.textarea.set_block(textarea_block);
 
-    f.render_widget(Paragraph::new("left")
-        .block(menu_block),
-    layout[0]);
+    const MENU_LISTING_HEIGHT: u16 = 2;
 
-    f.render_widget(model.textarea.widget(),
-        layout[1]);
+    let menu_listings: usize = (layout[0].as_size().height / MENU_LISTING_HEIGHT - 1).into();
+
+    let menu_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Length(MENU_LISTING_HEIGHT); menu_listings])
+        .split(menu_block.inner(layout[0]));
+
+    if let Some(selected_entries) = list_entries(model.calendar_state.selected_date) {
+        for entry in Iterator::enumerate(selected_entries) {
+            if let (idx, Ok(file_data)) = entry {
+                if let Some(file_name) = file_data.file_name().to_str() {
+                    let mut entry_block = Block::new();
+                    if model.selected_listing == idx {
+                        entry_block = entry_block.style(Style::new().bg(Color::LightBlue));
+                    }
+                    f.render_widget(
+                        Paragraph::new(file_name).block(entry_block).centered(),
+                        menu_layout[idx],
+                    );
+                }
+                model.num_listings = idx + 1;
+            }
+        }
+    }
+
+    menu_block = menu_block.title(format!(
+        "{date} - {listings} entries",
+        date = format_date(model.calendar_state.selected_date),
+        listings = model.num_listings
+    ));
+    f.render_widget(menu_block, layout[0]);
+    f.render_widget(model.textarea.widget(), layout[1]);
 
     if model.calendar_state.enabled {
-
-        let mut cal_event_store = CalendarEventStore::today(Style::new().light_blue().add_modifier(Modifier::UNDERLINED));
-        cal_event_store.add(model.calendar_state.selected_date, Style::new().red().bold());
-
-
+        let mut cal_event_store =
+            CalendarEventStore::today(Style::new().light_blue().add_modifier(Modifier::UNDERLINED));
+        cal_event_store.add(
+            model.calendar_state.selected_date,
+            Style::new().yellow().bold(),
+        );
 
         let area = Rect {
             width: 30,
             height: 12,
-            x: 5, 
-            y: f.size().height - 12
+            x: 5,
+            y: f.size().height - 12,
         };
 
-        let cal_block = Block::bordered()
-            .border_type(BorderType::Rounded);
+        let cal_block = Block::bordered().border_type(BorderType::Rounded);
 
-        let cal = 
-            Monthly::new(model.calendar_state.selected_date, cal_event_store)
-                .block(cal_block.clone())
-                .show_month_header(Style::new().bold());
-        
+        let cal = Monthly::new(model.calendar_state.selected_date, cal_event_store)
+            .block(cal_block.clone())
+            .show_month_header(Style::new().bold());
+
         let inner_area = cal_block.inner(area);
 
         f.render_widget(cal, inner_area);
-    
     }
 }
-
-
-
